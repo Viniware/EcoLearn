cab3395815011e7f20713e0e626e39a013df383d
Matheus da Silva <matheussimoesdasilva2002@gmail.com>
2024-10-05 18:22:31 -0300

    Working on the routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -40,6 +40,38 @@ router.post('/login', async (req, res) => {
             return res.status(401).json({ message: 'Wrong password'});
         }
 
-        res.status(200.json({ message:}))
+        res.status(200).json({ message: 'Logged in successfully!', user });
+    } catch (err) {
+        res.status(500).json({ message: 'Error when logging in', error: err.message });
+    }
+});
+
+router.get('/:userId', async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found'});
+        }
+
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json({ message: 'Error when fetching user', error: err.message});
+    }
+});
+
+router.put('/:userId', async (req, res) => {
+    const { userId } = req.params;
+    const updateData = req.body;
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(userId, updateData, { new: true});
+
+        if (!updatedUser) {
+            return res.status(404.json({message: 'User not found'}));
+        }
+        
     }
 })
