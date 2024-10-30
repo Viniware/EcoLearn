2c93c6541c0d46f6fa8ae17a7095b9118d1c3dc3
Matheus da Silva <matheussimoesdasilva2002@gmail.com>
2024-10-30 16:33:55 -0300

    Project pivot to Java Spring

---
+++ ecolearn/src/main/java/com/viniware/ecolearn/EcolearnApplication.java
@@ -0,0 +1,13 @@
+package com.viniware.ecolearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcolearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcolearnApplication.class, args);
+	}
+
+}
---
+++ ecolearn/src/test/java/com/viniware/ecolearn/EcolearnApplicationTests.java
@@ -0,0 +1,14 @@
+package com.viniware.ecolearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcolearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
