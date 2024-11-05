0d8df8bbd69024d7b17cde68b66bc9f5be2e4156
Matheus da Silva <matheussimoesdasilva2002@gmail.com>
2024-11-05 13:22:05 -0300

    Haskell Version Setup

--- server/routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createUser } = require('../controllers/user.controller');
-
-router.post('/users', createUser);
-
-module.exports = router;
