09f064a9a814c3b8bfe6241c4e272045cdfa765e
Vinicius <161797273+ViniciusGGomes@users.noreply.github.com>
2024-11-27 01:03:01 +0000

    alterei a frase do arquivo `PracticarPage.tsx`

--- frontend/src/pages/PracticarPage.tsx
+++ frontend/src/pages/PracticarPage.tsx
@@ -1,12 +1,16 @@
 // src/pages/PracticarPage.tsx
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import "./PraticarPage.css";
 
 const PracticarPage: React.FC = () => {
   return (
     <div>
-      <h1>Vamos Praticar!</h1>
+      <Sidebar></Sidebar>
+      <h1>
+        ------------------------------------------------------Vamos Praticar!
+      </h1>
       {/* Link para navegar até a página do Quizz */}
       <Link to="/quizz">
         <button>Quizz</button>
