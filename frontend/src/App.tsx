00703424c84b0bc327ccb08cb97790a96b902ae0
Vinicius <161797273+ViniciusGGomes@users.noreply.github.com>
2024-11-27 15:49:35 +0000

    Link no botão da pagina `Home` vai para as paginas `Sections` e `Practicar`

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import UserRegistrationPage from "./pages/UserRegistrationPage";
 import Header from "./components/Header";
 import PracticarPage from "./pages/PracticarPage";
-import AprenderPage from "./pages/AprenderPage";
 import ArtigosPage from "./pages/ArtigosPage"; // Importando a nova página de artigos
 import Loja from "./pages/Loja";
 import Artigo from "./pages/Artigo";
+import Sections from "./pages/Sections";
 
 const App: React.FC = () => {
   return (
@@ -18,13 +17,14 @@ const App: React.FC = () => {
           <div className="main-content" style={{ padding: "20px", flex: 1 }}>
             <Routes>
               <Route path="/" element={<HomePage />} />
-              <Route path="/register" element={<UserRegistrationPage />} />
               <Route path="/praticar" element={<PracticarPage />} />
               <Route path="/aprender" element={<HomePage />} />
               <Route path="/artigos" element={<ArtigosPage />} />{" "}
               <Route path="/Loja" element={<Loja />} />
               <Route path="/" element={<ArtigosPage />} />
               <Route path="/artigo" element={<Artigo />} />
+              <Route path="/" element={<HomePage />} />
+              <Route path="/sections" element={<Sections />} />
               {/* Adicionando a rota Artigos */}
             </Routes>
           </div>
